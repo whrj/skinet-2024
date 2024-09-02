3df8d6d77516c006a812a50fd417b222712eefba
William <wreid321980@hotmail.com>
2024-09-02 18:28:59 +0200

    End of Seeding Data

--- API/Program.cs
+++ API/Program.cs
@@ -23,4 +23,17 @@ var app = builder.Build();
 
 app.MapControllers();
 
+try
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+    throw;
+}
 app.Run();
