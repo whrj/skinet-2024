657f0ee2beac955e9b5695e0210f1ccb46803448
William <wreid321980@hotmail.com>
2024-09-18 11:43:21 +0200

    New Generic Repository

---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using Core.Entities;
+namespace Core.Interfaces;
+
+public interface IGenericRepository<T> where T:BaseEntity
+{
+    Task<T?> GetByIdAsync(int Id);
+    Task<IReadOnlyList<T>> ListAllAsync();
+    
+    void Add(T entity);
+
+    void Update(T entity);
+
+    void Remove(T entity);
+
+    Task<bool> SaveAllAsync(T entity);
+
+    bool Exists(int id);
+}
---
+++ Infrastructure/Data/GenericRepository.cs
@@ -0,0 +1,43 @@
+using System;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+namespace Infrastructure.Data;
+
+public class GenericRepository<T>(StoreContext context) : IGenericRepository<T?> where T : BaseEntity
+{
+    public void Add(T? entity)
+    {
+        context.Set<T>().Add(entity);
+    }
+
+    public bool Exists(int id)
+    {
+        return context.Set<T>().Any(x =>x.Id == id);
+    }
+
+    public async Task<T?> GetByIdAsync(int id)
+    {
+        return await context.Set<T>().FindAsync(id);
+    }
+
+    public async Task<IReadOnlyList<T?>> ListAllAsync()
+    {
+        return await context.Set<T>().ToListAsync();
+    }
+
+    public void Remove(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> SaveAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
